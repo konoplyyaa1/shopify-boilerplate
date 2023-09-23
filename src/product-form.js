330661df27be02ebdc9aeaae3a67fa560eb97bde
Konoplyyaa1 <conoplevictorr@gmail.com>
2023-09-23 19:18:47 +0300

    feat: update theme scripts, init GSAP

--- src/product-form.js
+++ src/product-form.js
@@ -1,36 +1,44 @@
+import gsap from 'gsap'
 export default class ProductForm extends window.HTMLElement {
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.form = this.querySelector('form')
-    this.button = this.querySelector('[type="submit"]')
-    this.form.querySelector('[name=id]').disabled = false
-    this.form.addEventListener('submit', this.submitHandler.bind(this))
-  }
+		this.form = this.querySelector('form')
+		this.button = this.querySelector('[type="submit"]')
+		this.form.querySelector('[name=id]').disabled = false
+		this.form.addEventListener('submit', this.submitHandler.bind(this))
+		this.button.addEventListener('mouseenter', this.hoverHandler.bind(this))
+	}
 
-  async submitHandler(event) {
-    event.preventDefault()
-    // Hint ✦ 2 change this ;)
-    this.button.classList.add('animate-pulse')
+	hoverHandler() {
+		gsap.to(this.button, {
+			duration: 0.5,
+			scale: 1.05,
+		})
+	}
 
-    const config = (type = 'javascript') => ({
-      method: 'POST',
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        Accept: `application/${type}`
-      },
-      // We get the data from the product form it self
-      body: new FormData(this.form)
-    })
+	async submitHandler(event) {
+		event.preventDefault()
+		this.button.classList.add('loading')
 
-    // We send a request to the ajax API
-    // More here https://shopify.dev/docs/api/ajax
-    try {
-      await fetch(`${window.routes.cart_add_url}`, config())
-      // We redirect to checkout
-      window.location = window.shopUrl + '/checkout'
-    } catch (error) {
-      console.error(error)
-    }
-  }
+		const config = (type = 'javascript') => ({
+			method: 'POST',
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+				Accept: `application/${type}`,
+			},
+			// We get the data from the product form it self
+			body: new FormData(this.form),
+		})
+
+		// We send a request to the ajax API
+		// More here https://shopify.dev/docs/api/ajax
+		try {
+			await fetch(`${window.routes.cart_add_url}`, config())
+			// We redirect to checkout
+			window.location = window.shopUrl + '/checkout'
+		} catch (error) {
+			console.error(error)
+		}
+	}
 }
